58b41ef0efe742d93d0bab50d6dc129bc1db4a7f
Victoria Hale <vic.hale74@gmail.com>
2019-11-26 02:28:40 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -39,7 +39,7 @@ class OwnersController < ApplicationController
       @owner = Owner.find(params[:id])
       @owner.update(params["owner"])
       if !params["pet"]["name"].empty?
-        @owner.pets << Pet.create(name: params["pet"]["name"])
+        @owner.pets << Pet.create(name: params[:pet][:name])
       end
       redirect "owners/#{@owner.id}"
   end
